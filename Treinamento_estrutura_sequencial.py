883e012cfe19018b62341fa73b263ef6e6bb64a1
Felipe Emilio <felipeeqa3@gmail.com>
2022-09-15 12:03:57 -0300

    Atualização de exercicios 15/09/2022 12:03

---
+++ Treinamento_estrutura_sequencial.py
@@ -0,0 +1,39 @@
+'''
+# 1) exiba uma string  
+    print("ola mundo!")
+
+# 2)  exiba o numero
+    Numero = input("Favor digite um numero de 1 ate 10:  ")
+    print(Numero)
+
+# 3) soma de notas 
+    numero01 = int (input("digite o primeiro numero: "))
+    numero02 = int (input("digite o segundo numero: "))
+    soma = numero01+numero02
+    print(soma)
+
+# 4) Media de notas  
+    Nota1 = int (input("Digite a primeira nota: "))
+    Nota2 = int (input("Digite a segunda nota: "))
+    Nota3 = int (input("Digite a terceira nota: "))
+    Nota4 = int (input("Digite a quarta nota: "))
+
+media = (Nota1 + Nota2 + Nota3 + Nota4) / 4
+print(media)
+
+# 5) metro para centimetro 
+    mt = float(input("Digite a metragem desejada:  "))
+    cm = (mt * 100)
+    print("Valor convertido para centimetro:  ",cm,"cm")
+
+# 6) area de um circulo
+    diametro = float(input("Qual é o diametro do circulo?  "))
+    raio = diametro / 2
+    area = (raio * raio) * 3.14
+    print(area)
+
+# 7) area de um quadrado
+lado = float(input("digite o lado do quadrado:  "))
+area = lado * lado
+print(area)
+'''
